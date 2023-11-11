f8cd08b736699e5eeebe875d52efa63d2e06c273
nadir-ahdid-vinci <nadir.ahdid@student.vinci.be>
2023-11-11 13:54:40 +0100

    test encore et encore

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,9 @@ import './stylesheets/main.css';
 
 import Navbar from './Components/Navbar/Navbar';
 import Router from './Components/Router/Router';
-import HomePage from './Components/Pages/HomePage';
+
+
 
 Navbar();
-HomePage();
+
 Router();
