bb7c4d7ba2321f771f46908fae59f98f7ecf1114
julien-remmery-vinci <julien.remmery@student.vinci.be>
2023-11-13 21:49:02 +0100

    fix: app.js

--- api/app.js
+++ api/app.js
@@ -9,10 +9,16 @@ const corsOptions = {
 
 const app = express();
 
+const usersRouter = require('./routes/users');
+const authsRouter = require('./routes/auths');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use('/users', usersRouter);
+app.use('/auths', authsRouter);
 
 app.use(cors(corsOptions));
 
