5f895250051753c4b632230f74fbbb74162683d7
nadir-ahdid-vinci <nadir.ahdid@student.vinci.be>
2023-11-13 21:41:44 +0100

    Nadir : Login form

---
+++ frontend/src/Components/Footer/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    const footer = document.querySelector('footer');
+    const footerContent = 
+    `
+        <p>IDHSUIHDUDHUIZHDUIDHUIZDHUH</p>
+    `;
+    footer.innerHTML = footerContent;
+}
+
+export default Footer;
