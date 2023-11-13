0d20342dbcacf8f3ded4e6c88efe1abf0e407a69
Nadir Ahdid <nadir.ahdid@student.vinci.be>
2023-11-13 14:14:25 +0100

    frontend navbar v1

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import { Navbar as BootstrapNavbar } from 'bootstrap';
+import logoImage from '../../img/logo.png'
 
 /**
  * Render the Navbar which is styled by using Bootstrap
@@ -8,25 +9,25 @@ import { Navbar as BootstrapNavbar } from 'bootstrap';
  * - the router will show the Page associated to this URI when the user click on a nav-link
  */
 
+ 
+
+
 const Navbar = () => {
   const navbarWrapper = document.querySelector('#navbarWrapper');
   const navbar = 
 
   `
-  <nav>
-  <div id="navbar">
     <div id="logo">
-      <img src="./img/logo.png" style="width: 100px;">
+      <img src="${logoImage}" style="width: 100px;">
     </div>
     <div id="content-nav-bar">
       <ul>
-        <li><a href="#" data-uri="/"></a>Home page</li>
+        <li><a href="#" data-uri="/">Home page</a></li>
+        <li><a href="#" data-uri="/login">Sign In</a></li>
+        <li><a href="#" data-uri="/register">Sign Up</a></li>
       </ul>
     </div>
-  </div>
-  </nav>
-
-  `;
+`;
   navbarWrapper.innerHTML = navbar;
 };
 
