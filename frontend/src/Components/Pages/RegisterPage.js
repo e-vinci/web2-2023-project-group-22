96e474123b631a69903ba136166fc94c64d02d7e
nadir-ahdid-vinci <nadir.ahdid@student.vinci.be>
2023-11-14 18:07:27 +0100

    test login Page

--- frontend/src/Components/Pages/RegisterPage.js
+++ frontend/src/Components/Pages/RegisterPage.js
@@ -7,26 +7,31 @@ const Register = () => {
     <!-- Button trigger modal -->
     <div id="login-form">
       <div class="container" id="container">
-
-        <div class="form-container sign-up-container">
-          <form action=".form-container sign-in-container">
-            <h1>Create Account</h1>
+        <div class="form-container sign-in-container">
+          <form action="#">
+            <h1>Sign Up</h1>
             <div class="social-container">
-              <a href="#" class="social"><i class="fa fa-google" aria-hidden="true"></i></a>
+              <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+              <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+              <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
             </div>
-            <span>or use your email for registration</span>
-            <input type="text" placeholder="Name" />
+            <span>or use your account</span>
+            <input type="text" placeholder="Prénom" />
+            <input type="text" placeholder="Nom" />
+            <input type="date" placeholder="Date de naissance" />
             <input type="email" placeholder="Email" />
             <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Confirm password" />
+            <a href="#">Forgot your password?</a>
             <button>Sign Up</button>
           </form>
         </div>
 
         <div class="overlay-container">
           <div class="overlay">
-            <div class="overlay-panel overlay-left">
-              <h1>Welcome Back!</h1>
-              <p>To keep connected with us please login with your personal info</p>
+            <div class="overlay-panel overlay-right">
+              <h1>Hello, Friend!</h1>
+              <p>Enter your personal details and start journey with us</p>
               <button class="ghost" id="signIn">Sign In</button>
             </div>
           </div>
