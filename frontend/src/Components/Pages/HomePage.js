d5402815a4824b05f9b8273a19343cbcaf989213
nadir-ahdid-vinci <nadir.ahdid@student.vinci.be>
2023-11-22 22:24:46 +0100

    homePage update

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,11 +1,12 @@
 import { clearPage } from '../../utils/render';
 import Navigate from '../Router/Navigate';
-import videoPresentation from '../../assets/paqueta.mp4';
+// import videoPresentation from '../../assets/paqueta.mp4';
+import logoImageNoir from '../../img/imageVille.jpg'
 
 const HomePage = () => {
   clearPage();
-  // presentationBlockImageText();
-  presentationVideoBlock();
+  presentationFirstBloc();
+  // HomePageSecondBlock();
   displayTripButtons();
   
 };
@@ -30,36 +31,33 @@ function displayTripButtons(){
 
 }
 
-function presentationVideoBlock(){
+function presentationFirstBloc(){
   const main = document.querySelector('main');
-  const createDivPresentation = document.createElement('div');
-  createDivPresentation.id = "presentationVideoDiv";
-  const presentationVideo = `<video width="320" height="240" autoplay muted>
-                                <source src="${videoPresentation}" type="video/mp4">
-                            </video>`
-  createDivPresentation.innerHTML = presentationVideo;
-  main.appendChild(createDivPresentation);
+  const createDivFirstBlock = 
+  `
+      <div id="presentationFirstBlock">
+        <div id="presentationText">
+          <h3>Voyagez avec Where2go</h3>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <button>Appuiez ici</button>
+        </div>
+        <div id="presentationImage">
+          <img src="${logoImageNoir}"alt="">
+        </div>
+      </div>
+  `
+  main.innerHTML = createDivFirstBlock;
 }
 
-
-// function presentationBlockImageText(){
+// function HomePageSecondBlock(){
 //   const main = document.querySelector('main');
-//   const createDivPresentationBlock = document.createElement('div');
-//   createDivPresentationBlock.id = "presentationBlock";
-//   createDivPresentationBlock.innerText = "hahahalefrrttropnul";
-  
-//   const createDivPresentationImage = document.createElement('div');
-//   createDivPresentationImage.id = "presentationImage";
-//   createDivPresentationImage.innerText = "hahahalefrrttropnul";
-
-//   const createDivPresentationText = document.createElement('div');
-//   createDivPresentationText.id = "presentationText";
-//   createDivPresentationText.innerText = "hahahalefrrttropnul";
-
-//   createDivPresentationBlock.appendChild(createDivPresentationImage);
-//   createDivPresentationBlock.appendChild(createDivPresentationText);
-//   main.appendChild(createDivPresentationBlock);
+//   main.innerHTML = 'ighrtighiugh';
 // }
+
+
+
+
+
 
 
 
