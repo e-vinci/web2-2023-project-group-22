0786f9fba767da4bb9e913343bac9e87f07a99e5
julien-remmery-vinci <julien.remmery@student.vinci.be>
2023-12-01 17:50:13 +0100

    add: create trip and gettrip by id

--- api/models/trips.js
+++ api/models/trips.js
@@ -1,14 +1,31 @@
 const client = require('./db_connection');
 
-async function test() {
+async function createTrip(countryCode, startDate, endDate) {
   const query = {
-    text: 'SELECT country_name FROM projetweb.countries WHERE country_code = $1',
-    values: ['BEL'],
+    text: 'INSERT INTO projetweb.trips (country_code, start_date, end_date) VALUES (?, ?, ?)',
+    values: [countryCode, startDate, endDate],
+  };
+  await client.query(query);
+
+  const trip = {
+    countryCode,
+    startDate,
+    endDate,
+  };
+
+  return trip;
+}
+
+async function getTrip(id) {
+  const query = {
+    text: 'SELECT country_code, start_date, end_date FROM projetweb.trips WHERE id_trip = $1',
+    values: id,
   };
   const res = await client.query(query);
   return res;
 }
 
 module.exports = {
-  test,
+  createTrip,
+  getTrip,
 };
