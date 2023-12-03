2a44b6201ec78fe9f59f55df709e474cbd8e8c20
Henry Obedekah <henry.obedekah@student.vinci.be>
2023-12-03 22:21:26 +0100

    date picker à voir en cours

--- frontend/src/Components/Pages/Trips/NewTripPage.js
+++ frontend/src/Components/Pages/Trips/NewTripPage.js
@@ -1,25 +1,32 @@
 import { clearPage } from "../../../utils/render";
-import Navigate from "../../Router/Navigate";
+ // import Navigate from "../../Router/Navigate";
 
 const NewTripPage = () => {
   const country = JSON.parse(localStorage.getItem('countryData'));
   localStorage.removeItem('countryData');
   clearPage();
   const main = document.querySelector('main');
+  document.head.innerHTML += `
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+
+  `;
 
   const formDestination =`
+
   <div id="form"> 
     <div class="container" id="container">
       <div class="destination-container" id ="container-destination">
         <form>
           <h1>Chose a destination </h1>
           <span>Where to go ? </span>
-            <input type="text" placeholder="ex. Paris, Japan, ..." id="destination" required/>
-            <div class="calendar"></div>
-            <h4>Start date </h4>
-            <input type="date" id="startDate" required/>
-            <h4>End date </h4>
-            <input type="date" id="endDate" required/>
+            <input type="text" placeholder="ex. Paris, Japan, ..." id="destination" required/>  
+
+           
+            <input type="text" name="datefilter" value="" />
+           
           <input type="submit" value="Start dreaming" id="createDest"/>
         </form>
       </div>
@@ -40,54 +47,74 @@ const NewTripPage = () => {
       </div>
     </div>
   </div>
-  `;
+  `; 
+
+ 
   main.innerHTML = formDestination;
+
+
+//   const datePickerRange  = `
+
+//   <script>
+// $(function() {
+//   $('input[name="daterange"]').daterangepicker({
+//     opens: 'left'
+//   }, function(start, end, label) {
+//     console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+//   });
+// });
+// </script>
+
+//   `;
+//   main.innerHTML = datePickerRange;
+
 
   if(country) {
     const destinationField = document.getElementById('destination');
     destinationField.value = country.name.common;
   }
 
-  const signInButton = document.getElementById('signIn');
-  signInButton.addEventListener('click', () => {
-    Navigate('/auth')
-  });
+  // const signInButton = document.getElementById('signIn');
+  // signInButton.addEventListener('click', () => {
+  //   Navigate('/auth')
+  // });
   
-  const submit = document.getElementById("createDest");
-  submit.addEventListener('click', async (event) => {
-    event.preventDefault();
-    if(!localStorage.getItem('user')) showLoginModal();
-    else{
-      event.preventDefault();
-      const destination = document.getElementById('destination').value;
-      const startDate = document.getElementById('startDate').value;
-      const endDate = document.getElementById('endDate').value;
-      await fetch('http://localhost:3000/trips/createtrip', {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-      body: JSON.stringify({
-        "countryCode": destination,
-        "startDate": startDate,
-        "endDate": endDate
-      })
-      })
-      .then((response) => {
-        if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
-        return response.json();
-      })
-      .then((result) => result);
-      Navigate("/modifytrip");
-    }
-  });   
-}
+//   const submit = document.getElementById("createDest");
+//   submit.addEventListener('click', async (event) => {
+//     event.preventDefault();
+//     if(!localStorage.getItem('user')) showLoginModal();
+//     else{
+//       event.preventDefault();
+//       const destination = document.getElementById('destination').value;
+//       const startDate = document.getElementById('startDate').value;
+//       const endDate = document.getElementById('endDate').value;
+//       await fetch('http://localhost:3000/trips/createtrip', {
+//       headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json',
+//       },
+//       method: 'POST',
+//       body: JSON.stringify({
+//         "countryCode": destination,
+//         "startDate": startDate,
+//         "endDate": endDate
+//       })
+//       })
+//       .then((response) => {
+//         if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
+//         return response.json();
+//       })
+//       .then((result) => result);
+//       Navigate("/modifytrip");
+//     }
+//   });   
+// }
 
-function showLoginModal(){
-  const modal = document.getElementById('signinmodal');
-  console.log(modal);
-  // modal.show();
+// function showLoginModal(){
+//   const modal = document.getElementById('signinmodal');
+//   console.log(modal);
+//   // modal.show();
+// }
 }
 
 export default NewTripPage;
