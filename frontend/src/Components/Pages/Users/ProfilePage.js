db73c3542e05263c13985f4c652a194e033717a7
lopesTiago <tiagoandre.amaral@student.vinci.be>
2023-12-17 01:16:24 +0100

    hhhh

--- frontend/src/Components/Pages/Users/ProfilePage.js
+++ frontend/src/Components/Pages/Users/ProfilePage.js
@@ -1,8 +1,12 @@
 
 import { clearPage } from '../../../utils/render';
 import image from '../../../img/usertest.png';
+import imagegif from '../../../img/globe-13.gif';
 import Navigate from '../../Router/Navigate';
 import Navbar from '../../Navbar/Navbar';
+
+
+
 
 
 
@@ -26,6 +30,12 @@ const ProfilPage = () => {
                             <div class="message ">
                             <p class="welcome-Profil"> Bienvenue </p> 
                             <p class="welcome-Nom-Profil"> ${userData.firstname}</p> </div>
+
+                            <img src='${imagegif}' width="450">
+
+                            <br>
+                          
+                            </br>
                             <input type="submit" class="button-input" id="logout" value="Log Out" />
                             </div>
                         </div>
@@ -81,91 +91,44 @@ const ProfilPage = () => {
                                         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3" class="active" aria-current="true"></button>
                                     </div>
                                     <div class="carousel-inner carouselDiv">
-                                        <div class="carousel-item carou">
-                                            <div class="cards-wrapper carou">
-                                                <div class="card" style="width: 18rem;">
-                                                    <img src="${image}" class="card-img-top" alt="...">
-                                                    <div class="card-body">
-                                                        <h5 class="card-title">Card title</h5>
-                                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                            <div class="carousel-item carou">
+                                                <div class="cards-wrapper wrapercasse">
+                                                    <div class="card" style="width: 18rem;">
+                                                        <img src="${image}" class="card-img-top" alt="...">
+                                                        <div class="card-body">
+                                                            <h5 class="card-title">Card title</h5>
+                                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div class="card" style="width: 18rem;">
-                                                    <img src="${image}" class="card-img-top" alt="...">
-                                                    <div class="card-body">
-                                                        <h5 class="card-title">Card title</h5>
-                                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                                                    </div>
-                                                </div>
-                                                <div class="card" style="width: 18rem;">
-                                                    <img src="${image}" class="card-img-top" alt="...">
-                                                    <div class="card-body">
-                                                        <h5 class="card-title">Card title</h5>
-                                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                                                    </div>
-                                                </div>
-                                            </div>  
-                                        </div>
-                                        <div class="carousel-item">
-                                            <div class="cards-wrapper">
-                                            <div class="card" style="width: 18rem;">
-                                            <img src="${image}" class="card-img-top" alt="...">
-                                            <div class="card-body">
-                                                <h5 class="card-title">Card title</h5>
-                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                                </div>  
                                             </div>
-                                        </div>
-                                        <div class="card" style="width: 18rem;">
-                                            <img src="${image}" class="card-img-top" alt="...">
-                                            <div class="card-body">
-                                                <h5 class="card-title">Card title</h5>
-                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                            <div class="carousel-item">
+                                                <div class="cards-wrapper">
+                                                        <div class="card" style="width: 18rem;">
+                                                            <img src="${image}" class="card-img-top" alt="...">
+                                                            <div class="card-body">
+                                                                <h5 class="card-title">Card title</h5>
+                                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                                            </div>
+                                                        </div>
+                                                    </div>  
                                             </div>
-                                        </div>
-                                        <div class="card" style="width: 18rem;">
-                                            <img src="${image}" class="card-img-top" alt="...">
-                                            <div class="card-body">
-                                                <h5 class="card-title">Card title</h5>
-                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                                            </div>
-                                        </div>
-                                            </div>  
-                                        </div>
                                         <div class="carousel-item active">
                                             <div class="cards-wrapper">
-                                            <div class="card" style="width: 18rem;">
-                                                    <img src="${image}" class="card-img-top" alt="...">
-                                                    <div class="card-body">
-                                                        <h5 class="card-title">Card title</h5>
-                                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                                                    </div>
-                                                </div>
                                                 <div class="card" style="width: 18rem;">
-                                                    <img src="${image}" class="card-img-top" alt="...">
-                                                    <div class="card-body">
-                                                        <h5 class="card-title">Card title</h5>
-                                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                                        <img src="${image}" class="card-img-top" alt="...">
+                                                        <div class="card-body">
+                                                            <h5 class="card-title">Card title</h5>
+                                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div class="card" style="width: 18rem;">
-                                                    <img src="${image}" class="card-img-top" alt="...">
-                                                    <div class="card-body">
-                                                        <h5 class="card-title">Card title</h5>
-                                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                                                    </div>
-                                                </div>
-                                            </div>  
+                                                </div>  
                                         </div>
                                     </div>
+                                
                                     
                                     </div>
                                     <button class="carousel-control-prev custom-prev-btn" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -193,18 +156,16 @@ const ProfilPage = () => {
                             </div>
 
                         </div>
-
-
                     </div>
                 </div>
-
             </div>
         </div> 
      </div>           
     `
-
     profilPage.innerHTML = profilPageForm;
+    showCountry();
     logout();
+    
 };
 
 function logout() {
@@ -217,5 +178,30 @@ function logout() {
 
 
 }
+
+function getTripDetails(tripId) {
+    fetch(`${process.env.API_BASE_URL}/trips/${tripId}`)
+        .then(responseFromServer => responseFromServer.json())
+        .then(data => {
+            const tripIds = data;
+            console.log(tripIds)
+
+           
+            
+        })
+}
+
+function showCountry() {
+    const userData = JSON.parse(localStorage.getItem('user'));
+    fetch(`${process.env.API_BASE_URL}/trips/user/${userData.email}`)
+        .then(responseFromServer => responseFromServer.json())
+        .then(data => {
+            const tripIds = data;
+            tripIds.map(trip => getTripDetails(trip.id_trip));
+            
+        })
+
+}
+
 
 export default ProfilPage;
